090b08689cd5db8545b8376106d0114f6b7389a5
Michanco Slesarev <michanco80@gmail.com>
2022-12-22 23:17:10 +0300

    adjust code Task09

--- Task09/Program.cs
+++ Task09/Program.cs
@@ -9,11 +9,21 @@
 int number = new Random().Next(10, 100);
 Console.WriteLine($"Случайное число из отрезка 10-99 ==> {number}");
 
-int firstDigit = number / 10;
-int secondDigit = number % 10;
+
 
 // if (firstDigit > secondDigit) Console.WriteLine($"Наибольшая цифра числа {firstDigit}");
 // else 
 
-int result = firstDigit > secondDigit ? firstDigit : secondDigit;
-Console.WriteLine($"Наибольшая цифра числа  {result}");
+
+//Console.WriteLine($"Наибольшая цифра числа  {result}");
+
+int maxDigit = MaxDigit(number);
+Console.WriteLine($"Наибольшая цифра числа ==> {maxDigit}");
+
+int MaxDigit(int number)
+{
+    int firstDigit = number / 10;
+    int secondDigit = number % 10;
+    int result = firstDigit > secondDigit ? firstDigit : secondDigit;
+    return result;
+}
