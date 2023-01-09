9fbecc74e94836e74f22e4827514e6d207d961aa
Michanco Slesarev <michanco80@gmail.com>
2023-01-09 23:11:35 +0300

    Add code task 19_2

--- Task19_2/Program.cs
+++ Task19_2/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 19_2. Напишите программу, которая 
+// принимает на вход  число и 
+// проверяет, является ли оно палиндромом.
+//  Выполнить с помощью числовых операций (целочисленное деление, остаток от деления).
+
+Console.WriteLine("Введите целое положительное число");
+long number = Convert.ToInt64(Console.ReadLine());
+
+Console.WriteLine(NumberRevers(number) == number ? "Да" : "Нет");
+
+long NumberRevers(long num)
+{
+    long numberRevers = 0;
+    while (num > 0)
+    {
+        long lastDigit = num % 10;
+        num = num / 10;
+        numberRevers = numberRevers * 10 + lastDigit;
+    }
+    return numberRevers;
+}
