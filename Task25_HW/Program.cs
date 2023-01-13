5bf5392e7bcb9652a47f67b1e23ff20ef49e54dd
Michanco Slesarev <michanco80@gmail.com>
2023-01-14 00:19:50 +0300

    add code Task25

---
+++ Task25_HW/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 25: Напишите цикл, который 
+// принимает на вход два числа (A и B) и 
+// возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.WriteLine("Введите число A ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите натуральное число B ");
+int degree = Convert.ToInt32(Console.ReadLine());
+
+if (degree > 0)
+{
+    int exponent = Exponent(number, degree);
+    Console.WriteLine($" Число {number} в степени {degree} равно {exponent}");
+}
+else Console.WriteLine($" Число {degree} не натуральное ");
+
+int Exponent(int num, int deg)
+{
+    int num1 = 1;
+    for (int i = 1; i <= deg; i++)
+    {
+        num1 *= num ;
+    }
+    return num1;
+}
+
+
