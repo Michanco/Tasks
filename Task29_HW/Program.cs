4fd8449667e0ed280c7ac3861ecb1e18258e4184
Michanco Slesarev <michanco80@gmail.com>
2023-01-15 02:59:28 +0300

    add code Task29

---
+++ Task29_HW/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 29: Напишите программу, которая 
+// задаёт массив из 8 элементов и 
+// выводит их на экран.
+
+// 1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
+
+Console.WriteLine("Введите интервал элементов массива");
+int min = Convert.ToInt32(Console.ReadLine());
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[] newArray = NewArray(min, max, 8);
+PrintArray(newArray);
+
+int[] NewArray(int min1, int max1, int size)
+{
+
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = rnd.Next(min1, max1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < arr.Length -1; i++)
+    {
+        Console.Write(arr[i] + ", ");
+    }
+    Console.Write(arr[arr.Length -1] + " ]");
+}
