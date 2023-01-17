67ab1fa39393f97b1dd9c0cbf9c8ec437982d2a2
Michanco Slesarev <michanco80@gmail.com>
2023-01-18 00:29:57 +0300

    add code Task36_HW

---
+++ Task36_HW/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 36: Задайте 
+// одномерный массив, 
+// заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int[] array = NewArrayRndInt(-10, 10, 6);
+PrintArray(array);
+int oddElementsOfArray = SumOddElementsOfArray(array);
+Console.Write($" -> {oddElementsOfArray}");
+
+int SumOddElementsOfArray(int[] arr)
+{
+    int sumOdd = arr[1];
+    for (int i = 3; i < arr.Length ; i++) 
+    {
+        if (i % 2 != 0 ) sumOdd += arr[i];
+    }
+    return sumOdd;
+}
+
+int[] NewArrayRndInt(int min1, int max1, int size)
+{
+
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = rnd.Next(min1, max1 + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        Console.Write(arr[i] + ", ");
+    }
+    Console.Write(arr[arr.Length - 1] + " ]");
+}
