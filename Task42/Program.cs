a34326a8b9f7538bbc72ddaef19a320fbb11f90a
Michanco Slesarev <michanco80@gmail.com>
2023-01-19 22:56:01 +0300

    add code Task42

---
+++ Task42/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 42: Напишите программу, которая будет преобразовывать
+// десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+int number = 48;
+string result = NumberBinary(number);
+Console.WriteLine($"{number} -> {result}");
+
+
+string NumberBinary(int num)
+{
+    string binar = string.Empty;
+    while (num > 0)
+    {   
+        binar = num % 2 + binar;
+        num /= 2;
+    }
+    return binar;
+}
+
+
+
+
+
