21f792d4db523dd3c6cf9552e4c454837213b05a
Michanco Slesarev <michanco80@gmail.com>
2023-01-26 02:11:25 +0300

    adj Methods

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -53,3 +53,58 @@ void PrintMatrix(int[,] matrix)
         Console.WriteLine();
     }
 }
+
+double[,] NewMatrixRndDouble(int rows, int columns, int min1, int max1)
+{
+
+    double[,] matrix = new double[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            double num = rnd.NextDouble() * (max1 - min1) + min1;
+            matrix[i, j] = Math.Round(num, 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrixDbl(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < matrix.GetLength(1) - 1; j++)
+        {
+            Console.Write($" {matrix[i, j],5} , ");
+        }
+        Console.Write($" {matrix[i, matrix.GetLength(1) - 1],5}  ] ");
+        Console.WriteLine();
+    }
+}
+
+double[] NewArrayRndDouble(int size, int min1, int max1)
+{
+
+    double[] arr = new double[size];
+    Random rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        double num = rnd.NextDouble() * (max1 - min1) + min1;
+        arr[i] = Math.Round(num, 1);
+    }
+    return arr;
+}
+
+void PrintArrayDbl(double[] arr)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        Console.Write(arr[i] + ", ");
+    }
+    Console.Write(arr[arr.Length - 1] + " ]");
+}
