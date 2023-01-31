ed681c4124a459b9e8ba59b202f6d3b0153b68d1
Michanco Slesarev <michanco80@gmail.com>
2023-02-01 00:15:05 +0300

    adj Methods

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -108,3 +108,18 @@ void PrintArrayDbl(double[] arr)
     }
     Console.Write(arr[arr.Length - 1] + " ]");
 }
+
+int[] MatrixToArray(int[,] matrix)
+{
+    int[] arr = new int[matrix.GetLength(0) * matrix.GetLength(1)];
+    int index = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(0); j++)
+        {
+            arr[index] = matrix[i, j];
+            index++;
+        }
+    }
+    return arr;
+}
