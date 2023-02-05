9f53b52f32b87fec140dd18258803f6e4e5a448f
Michanco Slesarev <michanco80@gmail.com>
2023-02-06 00:27:18 +0300

    add code Task67

---
+++ Task67/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 67: Напишите программу, которая будет принимать на вход число и
+// возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+// int FactorialRec(int n)
+// {
+// if (n == 1) return 1;
+// else return n * FactorialRec(n - 1);
+// }
+
+Console.WriteLine("ВВедите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int sumDigit = SumDigit(number);
+Console.WriteLine($"{sumDigit}");
+
+int SumDigit(int num)
+{
+    if (num == 0) return num;
+    else return num % 10 + SumDigit(num / 10);
+
+}
