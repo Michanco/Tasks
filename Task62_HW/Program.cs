b7db8a5a9db0e9529b30476c3350a540f265173c
Michanco Slesarev <michanco80@gmail.com>
2023-02-06 00:25:32 +0300

    adj code task62

--- Task62_HW/Program.cs
+++ Task62_HW/Program.cs
@@ -5,7 +5,7 @@
 // 11 16 15 06
 // 10 09 08 07
 
-int[,] masterMatrix = new int[4, 4];
+int[,] masterMatrix = new int[8, 5];
 NewSpiralMatrix(masterMatrix);
 PrintMatrix(masterMatrix);
 
@@ -28,32 +28,37 @@ void NewSpiralMatrix(int[,] matrix)
 {
     int end = matrix.GetLength(0) * matrix.GetLength(1);
     int temp = 1;
-    for (int i = 0; i < matrix.GetLength(0) - i; i++)
+    //for (int i = 0; i < matrix.GetLength(0) - i; i++)
+    int i = 0;
+    int d = 0;
+    while (i < matrix.GetLength(0) - i && d < matrix.GetLength(1) - d)
     {
-        for (int j = 0 + i; j < matrix.GetLength(1) - i; j++)
+        for (int j = 0 + i; j < matrix.GetLength(1) - d; j++)
         {
             matrix[i, j] = temp;
             temp++;
-            if (temp > end) goto Exit;
+            //if (temp > end) goto Exit;
         }
         for (int k = 1 + i; k < matrix.GetLength(0) - i; k++)
         {
             matrix[k, matrix.GetLength(1) - 1 - i] = temp;
             temp++;
-            if (temp > end) goto Exit;
+            //if (temp > end) goto Exit;
         }
-        for (int g = matrix.GetLength(1) - 2 - i; g >= i; g--)
+        for (int g = matrix.GetLength(1) - 1 - d; g > d; g--)
         {
-            matrix[matrix.GetLength(0) - 1 - i, g] = temp;
+            matrix[matrix.GetLength(0) - 2 - d, g] = temp;
             temp++;
-            if (temp > end) goto Exit;
+            //if (temp > end) goto Exit;
         }
         for (int q = matrix.GetLength(0) - 2 - i; q > i; q--)
         {
             matrix[q, i] = temp;
             temp++;
-            if (temp > end) goto Exit;
+            //if (temp > end) goto Exit;
         }
+        i++;
+        d++;
     }
-Exit:;
+    //Exit:;
 }
